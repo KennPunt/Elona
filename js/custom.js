f21866f55d354ad6ab57c9e16763d1f771f71513
Ken Vernaillen <k.vernaillen@sig.eu>
2023-11-19 15:25:41 +0100

    form

--- js/custom.js
+++ js/custom.js
@@ -5,43 +5,10 @@ document.addEventListener('DOMContentLoaded', function () {
 	});
 });
 
+gastenboekform
+
 var y = document.querySelector("#rsvp");
-/*,
-	b = document.querySelector('button[type="submit"]'),
-	_ = b.innerHTML,
-	w = new s.Validator(y, {
-		lang: s.enLang,
-		on: {
-			"validation:success": function () {
-				b.innerHTML = "Loading...";
-				fetch(y.getAttribute("action"), {
-					method: y.getAttribute("method"),
-					body: new FormData(y)
-				}).then(function (response) {
-					console.log("Response object:", response);
-					if (!response.ok) {
-						throw new Error('Network response was not ok ' + response.statusText);
-					}
-					return response.text();
-				}).then(function (text) {
-					console.log("Response text:", text);
-					if ("true" === text) {
-						b.innerHTML = "Email Sent";
-						y.reset();
-					} else {
-						b.innerHTML = "Error Sending";
-					}
-					setTimeout(function () {
-						b.innerHTML = _
-					}, 5000);
-				}).catch(function (error) {
-					console.error("Fetch error:", error);
-					b.innerHTML = "Error Sending";
-				});
-			}
-		}
-	});
-*/
+
 function disableform() {
 	var b = document.querySelector('#rsvp button');
 	b.innerHTML = "Verstuurd!";
@@ -50,8 +17,6 @@ function disableform() {
 	for (var i = 0; i < elements.length; i++) {
 		elements[i].disabled = true;
 	}
-
-
 }
 
 y.addEventListener("submit", function (t) {
