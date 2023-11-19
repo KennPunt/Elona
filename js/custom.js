f2ce378a9b9d4b37e8bbee3ad89835c698f9e2a8
Ken Vernaillen <k.vernaillen@sig.eu>
2023-11-19 15:16:26 +0100

    rsvp

--- js/custom.js
+++ js/custom.js
@@ -4,8 +4,9 @@ document.addEventListener('DOMContentLoaded', function () {
 		speed: 400
 	});
 });
-/*
-var y = document.querySelector("form"),
+
+var y = document.querySelector("#rsvp");
+/*,
 	b = document.querySelector('button[type="submit"]'),
 	_ = b.innerHTML,
 	w = new s.Validator(y, {
@@ -40,9 +41,38 @@ var y = document.querySelector("form"),
 			}
 		}
 	});
+*/
+function disableform() {
+	var b = document.querySelector('#rsvp button');
+	b.innerHTML = "Verstuurd!";
+
+	var elements = document.querySelectorAll('#rsvp input, #rsvp select, #rsvp textarea');
+	for (var i = 0; i < elements.length; i++) {
+		elements[i].disabled = true;
+	}
+
+
+}
 
 y.addEventListener("submit", function (t) {
-	w.validate(),
+	//w.validate(),
+	var b = document.querySelector('#rsvp button');
+	if(b.innerHTML == "Verstuurd!"){
+		alert("Je hebt de aanwezigheid reeds verstuurd.");
 		t.preventDefault();
+		return false;
+	}
+	this.submit();
+	disableform();
+	t.preventDefault();
+	return false;
+
+});
+
+/*
+document.querySelector('#rsvp').submit(function() {
+	this.submit();
+	disableFields(); // your own function
+	return false;
 });
 */
